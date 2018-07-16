dd30c1741eb486e77d2aad73482d72e294863dc6
Ortiz14 <Ortizalberto259@gmail.com>
2018-07-16 08:59:36 -0600

    fix issues

--- src/components/schedule/progressTracker.js
+++ src/components/schedule/progressTracker.js
@@ -1,10 +1,10 @@
-import react, {Component} from 'react';
+import React, { Component } from 'react';
 
-class ProgressTracker extends Component{
-    render(){
-        return(
+class ProgressTracker extends Component {
+    render() {
+        return (
             <div className="progress-tracker">
-                <label className="progress-tracker__label">Progress tracker</label>
+                <label className="progress-tracker__label">Progress Tracker</label>
                 <div className="progress-tracker__percentage">
                     0%
                 </div>
