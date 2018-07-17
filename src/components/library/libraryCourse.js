be460ac13cac23f3c9a3985a480a3a0228246754
Ortiz14 <Ortizalberto259@gmail.com>
2018-07-17 08:35:41 -0600

    fix issues and added/remove css classes in react

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -2,17 +2,29 @@ import React, { Component } from "react";
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
 
-import Action from '../action';
+import Icon from "../icon";
+import Arrow from "../arrow";
+import Action from "../action";
 
-class ScheduleCourse extends Component {
+class LibraryCourse extends Component {
   render() {
     return (
-      <div className="schedule-course">
-        <label className="schedule-course__label">{this.props.title}</label>
-        <Action onClick={() => this.props.toggleEnrolled(this.props.id)} className="schedule-course__action"/>
+      <div className="library-course">
+        <div className="library-course__title-check">
+          <label className="library-course__title">{ this.props.title }</label>
+          {Icon("fas fa-check", "library-course__icon")}
+        </div>
+        <Arrow className="library-course__arrow" />
+        <Action onClick={() => this.props.toggleEnrolled(this.props.id)} className="library-course__action"/>
+        <div className="library-course__description">
+          <label>Course Description</label>
+          <p>
+            { this.props.description }
+          </p>
+        </div>
       </div>
     );
   }
 }
 
-export default connect(null, actions)(ScheduleCourse);
+export default connect(null, actions)(LibraryCourse);
